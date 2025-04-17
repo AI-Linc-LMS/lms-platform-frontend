8ec9ee7a4cbe201e5fe3fe04e1a367a92d6fbbf9
dakshrajput45 <dakshr050@gmail.com>
2025-04-18 01:09:09 +0530

    Fix: Work on Responsiveness

--- src/features/learn/components/LeaderboardTable.tsx
+++ src/features/learn/components/LeaderboardTable.tsx
@@ -14,27 +14,27 @@ interface LeaderboardProps {
 const Leaderboard: React.FC<LeaderboardProps> = ({ data }) => {
   return (
     <div className="flex flex-col w-full lg:max-w-[270px] xl:max-w-[300px] transition-all duration-300">
-      <h2 className="text-xl font-semibold text-[#343A40] mb-2">
+      <h2 className="text-3xl font-semibold text-[#343A40] mb-3">
         Track Your Progress
       </h2>
-      <p className="text-xs text-[#495057] mb-4">
+      <p className="text-[14px] text-[#495057] mb-8">
         Keep grinding and stay top on our leaderboard
       </p>
 
       <div className="rounded-xl border border-gray-300 overflow-hidden ">
-        <table className="w-full text-center border-collapse min-h-[350px]">
+        <table className="w-full text-center border-collapse min-h-[450px]">
           <thead className="bg-gray-100">
-            <tr>
-              <th className="border-b border-r border-gray-300 px-2 py-2 text-xs text-gray-600">
+            <tr className="">
+              <th className="border-b border-r border-gray-300 px-2 py-5 text-[13px] text-gray-600">
                 Standing
               </th>
-              <th className="border-b border-r border-gray-300 px-2 py-2 text-xs text-gray-600">
+              <th className="border-b border-r border-gray-300 px-2 py-2 text-[13px] text-gray-600">
                 Name
               </th>
-              <th className="border-b border-r border-gray-300 px-2 py-2 text-xs text-gray-600">
+              <th className="border-b border-r border-gray-300 px-2 py-2 text-[13px] text-gray-600">
                 Course
               </th>
-              <th className="border-b border-gray-300 px-2 py-2 text-xs text-gray-600">
+              <th className="border-b border-gray-300 px-2 py-2 text-[13px] text-gray-600">
                 Marks
               </th>
             </tr>
@@ -49,16 +49,16 @@ const Leaderboard: React.FC<LeaderboardProps> = ({ data }) => {
                     isLast ? "bg-[#B4E0ED] text-[#264D64]" : "hover:bg-gray-50"
                   }`}
                 >
-                  <td className="border-b border-r border-gray-300 px-2 py-2 text-xs">
+                  <td className="border-b border-r border-gray-300 px-2 py-2 text-[13px]">
                     {item.standing}
                   </td>
-                  <td className="border-b border-r border-gray-300 px-2 py-2 text-xs">
+                  <td className="border-b border-r border-gray-300 px-2 py-2 text-[13px]">
                     {item.name}
                   </td>
-                  <td className="border-b border-r border-gray-300 px-2 py-2 text-xs">
+                  <td className="border-b border-r border-gray-300 px-2 py-2 text-[13px]">
                     {item.courseName}
                   </td>
-                  <td className="border-b border-gray-300 px-2 py-2 text-xs">
+                  <td className="border-b border-gray-300 px-2 py-4 text-[13px]">
                     {item.marks}
                   </td>
                 </tr>
@@ -67,11 +67,11 @@ const Leaderboard: React.FC<LeaderboardProps> = ({ data }) => {
           </tbody>
         </table>
       </div>
-      <div className="bg-[#DEE2E6] rounded-xl px-4 py-4 flex items-center gap-2 max-w-md mt-5">
+      <div className="bg-[#DEE2E6] rounded-xl px-4 py-5 flex items-center gap-2 max-w-md mt-12">
         <div className="mt-0.5 text-gray-500">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="w-4.5 h-4.5"
+            className="w-5 h-5"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
--- src/features/learn/components/referrals/Referrals.tsx
+++ src/features/learn/components/referrals/Referrals.tsx
@@ -4,13 +4,12 @@ import Referralsicon from "../../../../commonComponents/icons/referrals/Referral
 const Referrals = () => {
     return (
         <div className="w-[300px] rounded-3xl bg-white" >
-            <h1 className="">Referrals</h1>
-            <p className="font-normal font-sans text-[13px] text-[#495057]">Invite your friends and earn exciting rewards</p>
+            <h1 className="text-3xl font-semibold text-[#343A40] mb-3">Referrals</h1>
+            <p className="font-normal font-sans text-[14px] text-[#495057] mb-10">Invite your friends and earn exciting rewards</p>
             <div>
                 <Referralsicon />
-
             </div>
-            <button className="w-full h-[55px] bg-[#2A8CB0] rounded-2xl text-[16px] font-sans font-medium text-white">
+            <button className="w-full h-[55px] bg-[#2A8CB0] rounded-2xl text-[16px] font-sans font-medium text-white mt-2">
                 Copy Invitation Link
             </button>
 
