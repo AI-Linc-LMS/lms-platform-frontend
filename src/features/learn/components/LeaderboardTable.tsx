5c2afaba782be19fcf5d5b6d6daa5f9c5103cf45
soumic sarkar <soumic@codillion.com>
2025-04-18 18:25:26 +0530

    Update LeaderboardTable component to enhance table header and row styling, and truncate long course names for better readability

--- src/features/learn/components/LeaderboardTable.tsx
+++ src/features/learn/components/LeaderboardTable.tsx
@@ -21,49 +21,51 @@ const Leaderboard: React.FC<LeaderboardProps> = ({ data }) => {
         Keep grinding and stay top on our leaderboard
       </p>
 
-      <div className="rounded-xl border border-gray-300 overflow-hidden ">
+      <div className="rounded-xl border border-gray-300 overflow-hidden">
         <table className="w-full text-center border-collapse min-h-[450px]">
           <thead className="bg-gray-100">
-            <tr className="">
-              <th className="border-b border-r border-gray-300 px-2 py-5 text-[13px] text-gray-600">
-                Standing
-              </th>
-              <th className="border-b border-r border-gray-300 px-2 py-2 text-[13px] text-gray-600">
-                Name
-              </th>
-              <th className="border-b border-r border-gray-300 px-2 py-2 text-[13px] text-gray-600">
-                Course
-              </th>
-              <th className="border-b border-gray-300 px-2 py-2 text-[13px] text-gray-600">
-                Marks
-              </th>
-            </tr>
+        <tr>
+          <th className="border-b border-r border-gray-300 w-[60px] h-[50px] text-[12px] text-gray-600">
+            Standing
+          </th>
+          <th className="border-b border-r border-gray-300 w-[60px] h-[50px] text-[12px] text-gray-600">
+            Name
+          </th>
+          <th className="border-b border-r border-gray-300 w-[60px] h-[50px] text-[12px] text-gray-600">
+            Course
+          </th>
+          <th className="border-b border-gray-300 w-[60px] h-[50px] text-[12px] text-gray-600">
+            Marks
+          </th>
+        </tr>
           </thead>
           <tbody>
-            {data.map((item, index) => {
-              const isLast = index === data.length - 1;
-              return (
-                <tr
-                  key={index}
-                  className={`transition duration-200 ${
-                    isLast ? "bg-[#B4E0ED] text-[#264D64]" : "hover:bg-gray-50"
-                  }`}
-                >
-                  <td className="border-b border-r border-gray-300 px-2 py-2 text-[13px]">
-                    {item.standing}
-                  </td>
-                  <td className="border-b border-r border-gray-300 px-2 py-2 text-[13px]">
-                    {item.name}
-                  </td>
-                  <td className="border-b border-r border-gray-300 px-2 py-2 text-[13px]">
-                    {item.courseName}
-                  </td>
-                  <td className="border-b border-gray-300 px-2 py-4 text-[13px]">
-                    {item.marks}
-                  </td>
-                </tr>
-              );
-            })}
+        {data.map((item, index) => {
+          const isLast = index === data.length - 1;
+          return (
+            <tr
+          key={index}
+          className={`transition duration-200 ${
+            isLast ? "bg-[#B4E0ED] text-[#264D64]" : "hover:bg-gray-50"
+          }`}
+            >
+          <td className="border-b border-r border-gray-300 w-[70px] h-[50px] text-[12px]">
+            {item.standing}
+          </td>
+          <td className="border-b border-r border-gray-300 w-[70px] h-[50px] text-[12px]">
+            {item.name}
+          </td>
+            <td className="border-b border-r border-gray-300 w-[70px] h-[50px] text-[12px] p-1">
+            {item.courseName.length > 10
+              ? `${item.courseName.substring(0, 10)}...`
+              : item.courseName}
+            </td>
+          <td className="border-b border-gray-300 w-[70px] h-[50px] text-[12px]">
+            {item.marks}
+          </td>
+            </tr>
+          );
+        })}
           </tbody>
         </table>
       </div>
