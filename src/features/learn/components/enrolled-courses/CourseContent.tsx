c67558213394949f458365d532eb2ad4413f8413
soumic sarkar <soumic@codillion.com>
2025-04-18 13:30:10 +0530

    Enhance CourseCard and CourseContent components by adding teacher avatar display and updating trustedBy section to show avatars instead of text. Update mockCourses data to include new avatar images and adjust course types to accommodate teacher avatars.

--- src/features/learn/components/enrolled-courses/CourseContent.tsx
+++ src/features/learn/components/enrolled-courses/CourseContent.tsx
@@ -14,6 +14,17 @@ const CourseContent: React.FC<CourseContentProps> = ({ course }) => {
     <div className="w-full bg-white rounded-3xl p-6 shadow-sm">
       <h1 className="font-semibold text-[25px] font-sans">{course.title}</h1>
       <p className="text-[14px] text-[#6C757D] font-normal">{course.description}</p>
+      <div className="flex -space-x-2 mr-3">
+                    {course.teacherAvatar.slice(0, 5).map((avatar, index) => (
+                        <div key={index} className="w-12 h-12 rounded-full bg-gray-300 border-2 border-white overflow-hidden my-4">
+                            <img
+                                src={avatar || "/api/placeholder/32/32"}
+                                alt="Student avatar"
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                    ))}
+                </div>
       <CourseStatistics />
       <CourseActions />
       
