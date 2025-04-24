d815a2a545841ca6a051e7b988c41451bcf5a847
dakshrajput45 <dakshr050@gmail.com>
2025-04-24 14:33:55 +0530

    feat: minor animation in see all button

--- src/features/learn/components/continue-learning/ContinueCourses.tsx
+++ src/features/learn/components/continue-learning/ContinueCourses.tsx
@@ -1,9 +1,8 @@
 const ContinueCourses = () => {
   return (
-    <div className="flex flex-row items-center justify-between w-full ">
+    <div className="flex flex-row items-center justify-between w-full">
       <div>
-
-        <h1 className="text-[#343A40] font-bold text-[22px] font-sans ">
+        <h1 className="text-[#343A40] font-bold text-[22px] font-sans">
           Continue Learning
         </h1>
         <p className="text-[#6C757D] font-sans font-normal text-[18px]">
@@ -11,12 +10,12 @@ const ContinueCourses = () => {
         </p>
       </div>
       <div>
-        <button className="w-[95px] h-[55px] rounded-xl border border-[#2A8CB0] text-[15px] font-medium font-sans text-[#2A8CB0] cursor-pointer">
+        <button className="w-[95px] h-[55px] rounded-xl border border-[#2A8CB0] text-[15px] font-medium font-sans text-[#2A8CB0] cursor-pointer transition-all duration-200 hover:bg-[#E9F7FA] hover:text-[#1E7A99] hover:scale-95">
           See all
         </button>
       </div>
-
     </div>
   );
 };
+
 export default ContinueCourses;
