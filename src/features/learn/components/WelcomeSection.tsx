c21d12932c3164b7af011fa28d8dd3b38e2bec0e
soumic sarkar <soumic@codillion.com>
2025-05-09 11:18:57 +0530

    feat: implement route validation and redirection for invalid paths in App component

--- src/features/learn/components/WelcomeSection.tsx
+++ src/features/learn/components/WelcomeSection.tsx
@@ -3,10 +3,17 @@ import { RootState } from "../../../redux/store";
 
 const WelcomeSection = () => {
   const user = useSelector((state: RootState) => state.user);
-  
+
   return (
     <div className="mx-0">
-      <h1 className="text-2xl text-black font-sans font-bold md:text-[22px]">Welcome {user.full_name},</h1>
+      <h1 className="text-2xl text-black font-sans font-bold md:text-[22px]">
+        Welcome{" "}
+        <span className="inline-block group perspective-[1000px]">
+          <span className="relative inline-block transition-transform duration-500 transform group-hover:rotate-y-180 text-[#2A8CB0]">
+            {user.full_name}
+          </span>
+        </span>
+      </h1>
       <p className="text-[#495057] md:text-[18px] font-normal font-sans">
         Here is a glimpse of your overall progress.
       </p>
