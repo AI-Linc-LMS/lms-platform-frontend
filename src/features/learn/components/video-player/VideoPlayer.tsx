07404c620b600c0d4d632c3b5153e2633962aaf0
soumic sarkar <soumic@codillion.com>
2025-05-14 18:36:31 +0530

    Merge branch 'stagging' of github.com:AI-Linc-LMS/lms-platform-frontend into feature/video-seek-disable

--- src/features/learn/components/video-player/VideoPlayer.tsx
+++ src/features/learn/components/video-player/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import  { useState, useCallback, useEffect } from 'react';
 import { VideoPlayerProps } from './types';
 import { isVimeoUrl } from './utils/formatters';
 import { StandardPlayer } from './components/StandardPlayer';
