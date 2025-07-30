a01e5bcdf29ededeb38acdde20618b132f6e5e67
soumic sarkar <soumic@codillion.com>
2025-07-30 15:12:51 +0530

    Add Community page and update navigation links

---
+++ src/features/community/pages/CommunityPage.tsx
@@ -0,0 +1,10 @@
+const CommunityPage = () => {
+  return (
+    <div>
+      <h1>Community Page</h1>
+      {/* Add your community page content here */}
+    </div>
+  );
+}
+
+export default CommunityPage;
