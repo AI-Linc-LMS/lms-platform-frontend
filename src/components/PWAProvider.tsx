52488f3ff6d89650df4724154413b6e1f43b4883
VRUJESH H M <60779508+VRU2306@users.noreply.github.com>
2025-10-13 23:29:13 +0530

    feat: Add mobile device detection for conditional PWA install prompt display

--- src/components/PWAProvider.tsx
+++ src/components/PWAProvider.tsx
@@ -4,15 +4,25 @@ import { PWAInstallPrompt } from "./PWAInstallPrompt";
 import { OfflineIndicator } from "./OfflineIndicator";
 import PWASplashScreen from "./PWASplashScreen";
 
+// Helper function to detect if the device is mobile
+const isMobileDevice = () => {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+};
+
 export const PWAProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+  const showInstallPrompt = isMobileDevice();
+
   return (
     <>
       {/* Splash overlay for installed PWA launch with a brief loader */}
       <PWASplashScreen />
       {children}
-      <PWAInstallPrompt />
+      {/* Only show install prompt on mobile devices */}
+      {showInstallPrompt && <PWAInstallPrompt />}
       {/* <PWAUpdateNotification /> */}
       <OfflineIndicator />
     </>
