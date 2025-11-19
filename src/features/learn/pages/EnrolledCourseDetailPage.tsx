059677ad2523601d84d0d477bdcb523bf5013470
Utkarsh <utkarshsingx@users.noreply.github.com>
2025-11-19 07:35:24 +0530

    manage students well done message

--- src/features/learn/pages/EnrolledCourseDetailPage.tsx
+++ src/features/learn/pages/EnrolledCourseDetailPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import PrimaryButton from "../../../commonComponents/common-buttons/primary-button/PrimaryButton";
